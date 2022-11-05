03f5d2cbe905a69b5671dbec94a984a8fee4631d
Harlii <dirol779@rambler.ru>
2022-11-05 21:34:45 +0300

    FindDeletedValueFromArray

--- src/test/java/ru/java_examples/FindDeletedValueFromArray.java
+++ src/test/java/ru/java_examples/FindDeletedValueFromArray.java
@@ -1,5 +1,7 @@
 package ru.java_examples;
 
+import org.testng.Assert;
+
 import java.util.Arrays;
 
 public class FindDeletedValueFromArray {
@@ -7,23 +9,34 @@ public class FindDeletedValueFromArray {
   // Дан массив элементов от 1 до N. Удаляется один элемент, оставшиеся элементы перемешиваются. Найти удаленный элемент
 
   public static void main(String[] args) {
-    int[] arr = {1, 3, 5, 10, 12, 26, 30, 63, 103, 120}; // например, удалили число 30 и перемешали значения
-    int[] newArr = {103, 3, 12, 1, 5, 120, 26, 63, 10};
-    System.out.println(findDeletedValue(arr, newArr));
+    int[] arr = {3, 1, 2, 5}; // например, удалили число 4 и перемешали значения
+//    int[] newArr = {103, 3, 12, 1, 5, 120, 26, 63, 10};
+//    System.out.println(findDeletedValue(arr));
+    Assert.assertEquals(findDeletedValue(new int[]{2, 5, 4, 3}), 1);
+    Assert.assertEquals(findDeletedValue(new int[]{3, 1, 2, 5}), 4);
+    Assert.assertEquals(findDeletedValue(new int[]{3, 1, 2, 4}), 5);
+    Assert.assertEquals(findDeletedValue(new int[]{1}), 2);
+    Assert.assertEquals(findDeletedValue(new int[]{}), 1);
 
-    int[] arr_2 = {1, 3, 5, 10, 12, 26, 30, 63, 103, 120}; // например, удалили последнее число 120 и перемешали значения
-    int[] newArr_2 = {103, 3, 12, 1, 5, 30, 26, 63, 10};
-    System.out.println(findDeletedValue(arr_2, newArr_2));
+//    int[] arr_2 = {1, 3, 5, 10, 12, 26, 30, 63, 103, 120}; // например, удалили последнее число 120 и перемешали значения
+//    int[] newArr_2 = {103, 3, 12, 1, 5, 30, 26, 63, 10};
+//    System.out.println(findDeletedValue(arr_2, newArr_2));
   }
 
-  public static String findDeletedValue(int[] arr, int[] newArr) {
-    Arrays.sort(newArr);
-    for (int i = 0; i < arr.length - 2; i++) {
-      if (arr[i] != newArr[i]) {
-        return "Удаленное число: " + arr[i];
+  public static int findDeletedValue(int[] arr) {
+    if (arr.length == 0) {
+      return 1;
+    }
+    Arrays.sort(arr);
+    if (arr[0] != 1) {
+      return 1;
+    }
+    for (int i = 0; i < arr.length - 1; i++) {
+      if (arr[i+1] - arr[i] != 1) {
+        return arr[i] + 1;
       }
     }
-    return "Удаленное число: " + arr[arr.length-1];
+    return arr[arr.length-1] + 1;
   }
 
 }
