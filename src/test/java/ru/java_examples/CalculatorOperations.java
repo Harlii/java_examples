12a56ce1f5ba679b45ae9c91605e2bcb18779930
Harlii <dirol779@rambler.ru>
2022-11-05 16:28:46 +0300

    Calculator operations

---
+++ src/test/java/ru/java_examples/CalculatorOperations.java
@@ -0,0 +1,55 @@
+package ru.java_examples;
+
+import org.testng.Assert;
+
+import java.text.DecimalFormat;
+
+public class CalculatorOperations {
+
+  static DecimalFormat decimalFormat = new DecimalFormat("#.##");
+  // Можно искользовать String.format("%.3f", value) есди нам нужны конечные нули - 6.200
+
+  public static void main(String[] args) {
+    Assert.assertEquals(sum(3, 5), 8);
+    Assert.assertEquals(sum(2.9, 3.3), "6,2");
+    Assert.assertEquals(difference(12, 5), 7);
+    Assert.assertEquals(difference(5.9, 3.3),  "2,6");
+    Assert.assertEquals(multiplication(3, 5), 15);
+    Assert.assertEquals(multiplication(2.2, 4.3), "9,46");
+    Assert.assertEquals(division(10, 4), 2);
+    Assert.assertEquals(division(10.0, 4.0), "2,5");
+  }
+
+  public static int sum(int a, int b) {
+    return a + b;
+  }
+
+  public static String sum(double a, double b) {
+    return decimalFormat.format(a + b);
+  }
+
+  public static int difference(int a, int b) {
+    return a - b;
+  }
+
+  public static String difference(double a, double b) {
+    return decimalFormat.format(a - b);
+  }
+
+  public static int multiplication(int a, int b) {
+    return a * b;
+  }
+
+  public static String multiplication(double a, double b) {
+     return decimalFormat.format(a * b);
+  }
+
+  public static int division(int a, int b) {
+    return a / b;
+  }
+
+  public static String division(double a, double b) {
+     return decimalFormat.format(a / b);
+  }
+
+}
